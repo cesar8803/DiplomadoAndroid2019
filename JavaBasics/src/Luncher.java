b06f8f473f230c18dbf2fd9d6cb733473b9fd6f1
Cesar <cesar.guzman@mobilestudio.mx>
2019-01-12 10:48:46 -0600

    Se sube el proyecto de JavaBasics
    Es un proyecto para inicar en Java. Primera clase

---
+++ JavaBasics/src/Luncher.java
@@ -0,0 +1,44 @@
+public class Luncher {
+
+
+    public static void main(String args[]){
+
+        System.out.println("Hola!  K ace");
+
+        //Selecciono el tipo persona
+        //le coloco un nombre (referencia) a mi objeto
+        //Invoco al constructor de la clase
+        // Un constructor es una subrutina/funcion cuya misión
+        // es inicializar un objeto de una clase.
+        // En el constructor se asignan los valores iniciales del nuevo objeto.
+
+        Persona manuel = new Persona();
+
+        manuel.setNombre("Manuel Benjamin");
+        manuel.setEdad(32);
+        manuel.setEstatura(1.82);
+        manuel.setGenero(true);
+        manuel.setPeso(85);
+
+        String saludoManuel = manuel.quienSoy();
+
+        System.out.println(saludoManuel);
+
+
+
+        Persona christian = new Persona();
+
+        christian.setNombre("Christian");
+        christian.setEdad(38);
+        christian.setEstatura(1.69);
+        christian.setGenero(true);
+        christian.setPeso(75);
+
+        String saludoChris = christian.quienSoy();
+
+        System.out.println(saludoChris);
+
+
+    }
+
+}
---
+++ JavaBasics/src/Persona.java
@@ -0,0 +1,73 @@
+public class Persona {
+
+
+    private String nombre;
+    private String tonodepiel;
+    private int edad;
+    private boolean genero;
+    private String fechadenacimiento;
+    private double estatura;
+    private double peso;
+
+    // Declaración de un metodo (Representa una acción o comportamiento de una clase/ objeto)
+    public String quienSoy(){
+
+            return "Hola mi nombre es: "+nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTonodepiel() {
+        return tonodepiel;
+    }
+
+    public void setTonodepiel(String tonodepiel) {
+        this.tonodepiel = tonodepiel;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isGenero() {
+        return genero;
+    }
+
+    public void setGenero(boolean genero) {
+        this.genero = genero;
+    }
+
+    public String getFechadenacimiento() {
+        return fechadenacimiento;
+    }
+
+    public void setFechadenacimiento(String fechadenacimiento) {
+        this.fechadenacimiento = fechadenacimiento;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
