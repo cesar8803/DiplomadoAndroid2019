19af739dd591db3804c18018e8d44e36852445b9
Cesar <cesar.guzman@mobilestudio.mx>
2019-02-02 13:33:56 -0600

    Se sube conección entre el Controlador y la Vista
    La conección se hace mendiante Id

--- AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
+++ AndroidLayoutBasics/app/src/main/java/mx/mobilestudio/androidlayoutbasics/MainActivity.java
@@ -2,15 +2,47 @@ package mx.mobilestudio.androidlayoutbasics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button botonInicial;
+    private Button botonSecundario;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        botonInicial = findViewById(R.id.button1);
+
+        botonSecundario = findViewById(R.id.button2);
+
+
+        botonInicial.setOnClickListener(this);
+        botonSecundario.setOnClickListener(this);
 
 
     }
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button1:
+                Toast.makeText(this , "Se dio click en el boton 1", Toast.LENGTH_LONG).show();
+
+                break;
+            case R.id.button2:
+                Toast.makeText(this , "Se dio click en el boton 2", Toast.LENGTH_LONG).show();
+
+                break;
+        }
+
+    }
 }
