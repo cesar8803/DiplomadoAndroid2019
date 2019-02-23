42aa7474c6514da4c7ab904c7a498cf89f392847
Cesar <cesar.guzman@mobilestudio.mx>
2019-02-23 11:58:51 -0600

    Subo los cambios de la clase de hoy hasta medio dia
    Subo los cambios de la clase de hoy hasta medio dia

--- PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
+++ PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
@@ -1,5 +1,6 @@
 package mx.mobilestudio.placefinder;
 
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,7 +8,14 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener , Response.Listener , Response.ErrorListener {
 
     public SearchView searchView;
 
@@ -44,15 +52,48 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
 
         //Toast.makeText(this, query, Toast.LENGTH_LONG).show();
 
-
+        callFourSquareApi(query);
         return false;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
 
-        Toast.makeText(this, newText, Toast.LENGTH_LONG).show();
 
         return false;
+    }
+
+
+    public void callFourSquareApi(String query){
+
+        String location = "19.395209"+","+"-99.1544203"; // HARDCODE
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String URL = Uri.parse("https://api.foursquare.com/v2/venues/search").buildUpon()
+                     .appendQueryParameter("client_id","HOSIY11XMXHWFADXIPQTF5HRZA3YIWIFGRAOA5NIGXOY3CWI")
+                     .appendQueryParameter("client_secret","OGATJNY0E0JY15PRXYD5MQ2WW3EMFLRAWFHLAOQYSTMVKMHM")
+                     .appendQueryParameter("v","20130815")
+                     .appendQueryParameter("ll","19.433997,-99.146006")
+                     .appendQueryParameter("query",query).build().toString();
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL ,this,this);
+
+
+        queue.add(stringRequest);
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(this, error.getMessage(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onResponse(Object response) {
+
+        Toast.makeText(this, response.toString(), Toast.LENGTH_SHORT).show();
+        
     }
 }
