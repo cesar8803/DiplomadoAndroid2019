a5cc3cfbffcac4840dc7e2271020f5a9d8810520
Cesar <cesar.guzman@mobilestudio.mx>
2019-03-02 14:08:37 -0600

    Subimos el manejo de fragmentos
    Subimos el manejo de fragmentos

--- PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
+++ PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/MainActivity.java
@@ -1,11 +1,16 @@
 package mx.mobilestudio.placefinder;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -16,16 +21,27 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import mx.mobilestudio.placefinder.fragment.ListResultFragment;
+import mx.mobilestudio.placefinder.fragment.MapsResultsFragment;
 import mx.mobilestudio.placefinder.model.ApiFourSquareResponse;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener , Response.Listener , Response.ErrorListener {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener , Response.Listener , Response.ErrorListener, View.OnClickListener {
 
     public SearchView searchView;
+    public ImageButton imageButtonMap;
+
+    private FragmentManager fragmentManager; // Clase que me permite agregar fragmentos a mi Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButtonMap = findViewById(R.id.mybuttonmap);
+        imageButtonMap.setOnClickListener(this);
+// Inicializamos fragment manager
+        fragmentManager = getFragmentManager();
+
 
         configureToolBar();
     }
@@ -106,6 +122,54 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         Toast.makeText(this,  apiFourSquareResponse.response.venues.get(0).name, Toast.LENGTH_LONG).show();
         Toast.makeText(this,  String.valueOf(apiFourSquareResponse.response.venues.get(0).location.lat), Toast.LENGTH_LONG).show();
         Toast.makeText(this,  String.valueOf(apiFourSquareResponse.response.venues.get(0).location.lng), Toast.LENGTH_LONG).show();
+
+
+       attachFragment();
+        //attachMapFragment();
+    }
+
+
+
+    // Generamos un metodo para agregar (attachar) nuestros Fragmentos
+    public void attachFragment(){
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment listResultFragment = new ListResultFragment();
+
+        fragmentTransaction.replace(R.id.main_container, listResultFragment);  // Aqui es donde colocamos el fragmento
+
+        fragmentTransaction.commit();
+    }
+
+
+    // Generamos un metodo para agregar (attachar) nuestros Fragmentos
+    public void attachMapFragment(){
+       /// MapsResultsFragment
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment mapsResultsFragment = new MapsResultsFragment();
+
+        fragmentTransaction.replace(R.id.main_container, mapsResultsFragment);  // Aqui es donde colocamos el fragmento
+
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.main_container);
+
+        if( currentFragment instanceof  MapsResultsFragment){
+            attachFragment();
+            imageButtonMap.setBackgroundResource(android.R.drawable.btn_default);
+
+        }else if ( currentFragment instanceof  ListResultFragment){
+            attachMapFragment();
+            imageButtonMap.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+
+        }
+
 
 
     }
