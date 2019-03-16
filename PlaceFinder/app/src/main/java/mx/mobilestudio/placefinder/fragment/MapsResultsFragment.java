9d15bf43d9aca9b2ffb791ca70a7e3a8f993b77d
Cesar <hlj.cesar@gmail.com>
2019-03-16 14:22:42 -0600

    Se sube version con CameraUpdate
    Se sube version con CameraUpdate

--- PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/fragment/MapsResultsFragment.java
+++ PlaceFinder/app/src/main/java/mx/mobilestudio/placefinder/fragment/MapsResultsFragment.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 
@@ -15,7 +23,7 @@ import mx.mobilestudio.placefinder.model.Venue;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapsResultsFragment extends Fragment {
+public class MapsResultsFragment extends Fragment implements OnMapReadyCallback {
 
 
 
@@ -32,7 +40,13 @@ public class MapsResultsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_maps_results, container, false);
+        View view = inflater.inflate(R.layout.fragment_maps_results, container, false);
+
+        MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map_fragment);
+
+        mapFragment.getMapAsync(this);
+
+        return view;
     }
 
 
@@ -42,5 +56,18 @@ public class MapsResultsFragment extends Fragment {
 
     public void setVenues(ArrayList<Venue> venues) {
         this.venues = venues;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(getActivity(), "Mapa cargo exitosamente",Toast.LENGTH_LONG).show();
+
+        LatLng latLng = new LatLng(19.395209,-99.1544203);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,14f);
+
+        googleMap.moveCamera(cameraUpdate);
+
     }
 }
