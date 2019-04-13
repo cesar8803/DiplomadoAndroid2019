022eb9e245da842f7d87bde242b8c0a95296e39d
Cesar <hlj.cesar@gmail.com>
2019-04-13 13:57:49 -0500

    Update PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/PromoFormActivity.java

--- PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/PromoFormActivity.java
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/PromoFormActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import mx.mobilestudio.promohunters.fragment.InitialSelectionFragment;
 import mx.mobilestudio.promohunters.fragment.OnlineFormFragment;
 import mx.mobilestudio.promohunters.interfaces.onModeSelection;
 
