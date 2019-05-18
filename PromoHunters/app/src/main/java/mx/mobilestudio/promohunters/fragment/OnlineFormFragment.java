152231e58da7c87760757effb5e3531667b16ed9
Cesar <cesar.guzman@mobilestudio.mx>
2019-05-18 12:17:15 -0500

    Ultimos cambios para guardar la promo en firebase

--- PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/fragment/OnlineFormFragment.java
+++ PromoHunters/app/src/main/java/mx/mobilestudio/promohunters/fragment/OnlineFormFragment.java
@@ -3,6 +3,7 @@ package mx.mobilestudio.promohunters.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
 import mx.mobilestudio.promohunters.R;
 import mx.mobilestudio.promohunters.model.Promo;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class OnlineFormFragment extends Fragment implements View.OnClickListener {
+public class OnlineFormFragment extends Fragment implements View.OnClickListener , OnSuccessListener , OnFailureListener {
 
 
     Button buttonSavePromo;
@@ -26,11 +33,20 @@ public class OnlineFormFragment extends Fragment implements View.OnClickListener
     EditText editTextLink;
     EditText editTextDesc;
 
+    private FirebaseStorage firebaseStorage;// Almacenar archivos, imagenes, videos
+    private DatabaseReference databaseReference; // Para conectarse con realtime database de firebase
+
+
+
+
+
+
     Promo  promo;
 
 
     public OnlineFormFragment() {
         // Required empty public constructor
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 
 
@@ -114,8 +130,26 @@ public class OnlineFormFragment extends Fragment implements View.OnClickListener
         promo.setLink(editTextLink.getText().toString());
         promo.setDescription(editTextDesc.getText().toString());
 
+
+        String promoID = databaseReference.push().getKey();
+        databaseReference.child("promos").child(promoID).setValue(promo).addOnSuccessListener(this).addOnFailureListener(this);
+
     }
 
 
+    @Override
+    public void onSuccess(Object o) {
 
+        Toast.makeText(getActivity(), "Se guardo de forma exitosa!! ", Toast.LENGTH_LONG).show();
+        getActivity().finish();
+
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+        Toast.makeText(getActivity(), "Ocurrio un error!!  "+e.getStackTrace().toString(), Toast.LENGTH_LONG).show();
+
+
+    }
 }
